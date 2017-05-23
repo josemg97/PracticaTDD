2e62c0d57d60122f4c3b56f8736e30aa67ff03cc
Jose <jose_campillos_1997@hotmail.com>
2017-05-23 18:26:39 +0200

    green: Crear array asociativo con varios datos

--- src/AsocArray.java
+++ src/AsocArray.java
@@ -25,22 +25,46 @@ public class AsocArray {
 
 
 		public void put(String clave, String valor){
-		    primero = new Nodo();
-		    primero.clave = clave;
-		    primero.valor = valor;
-		    primero.siguiente = null;
-		    tam ++;
+		    
+		    if(tam == 0){
+		      primero = new Nodo();
+		      primero.clave = clave;
+		      primero.valor = valor;
+		      primero.siguiente = null;
+		      tam ++;
+		      
+		    }else{
+		      
+		      Nodo actual = primero;
+		      Nodo sig = actual.siguiente;
+		      
+		      while (sig != null) {
+		        actual = actual.siguiente;
+		        sig = actual.siguiente;
+		      }
+		      
+		       Nodo n = new Nodo();
+		       n.clave = clave;
+		       n.valor = valor;
+		       n.siguiente = null;
+		       tam ++;
+		       actual.siguiente = n;
+		       
+		      tam++;
+		    }
+
 		  }
-
-
-
+		  
 		  public String get(String clave){
-			    Nodo actual = primero;
-			    String val = "";
-			    if(actual!= null && actual.clave.equals(clave)){
-			      val = actual.valor;
-			    }
-			    return val;
-			  }
+		    Nodo actual = primero;
+		    String val = "";
+		    while(actual != null){
+		      if(actual!= null && actual.clave.equals(clave)){
+		        val = actual.valor;
+		      }
+		      actual = actual.siguiente;
+		    }
+		    return val;
+		  }
 
 }
